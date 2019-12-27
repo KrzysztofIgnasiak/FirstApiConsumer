cb6b678dc6d3bf093b34de3a44d57681b7cf7cc9
kkignasiak <kkignasiak.98@gmail.com>
2019-12-27 17:20:58 +0100

    adding display list of Users in AccountController

--- CosumeApi/Controllers/AccountController.cs
+++ CosumeApi/Controllers/AccountController.cs
@@ -11,11 +11,26 @@ namespace CosumeApi.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account/Users
+        //GET : Account/Users
         public async Task<ActionResult> Users()
         {
+            DisplayUserViewModel Model = new DisplayUserViewModel();
+           List <AccountDisplayBindingModel> Users = null;
+            HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync("Account");
+            if (response.IsSuccessStatusCode)
+            {
+                Model.Users = await response.Content.ReadAsAsync<List<AccountDisplayBindingModel>>();
+                //Users = await response.Content.ReadAsAsync<List<AccountDisplayBindingModel>>();
+            }
+            return View(Model);
+           
+        }
+
+        // GET: Account/User
+        public async Task<ActionResult> User()
+        {
             AccountDisplayBindingModel User = null;
-            HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync("Account/48097758-a085-4f3f-966b-dfa4e27ae803");
+            HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync("Account/a4a20198-5cea-439a-99a4-8e4c01d6324a");
             if (response.IsSuccessStatusCode)
             {
                 User = await response.Content.ReadAsAsync<AccountDisplayBindingModel>();
