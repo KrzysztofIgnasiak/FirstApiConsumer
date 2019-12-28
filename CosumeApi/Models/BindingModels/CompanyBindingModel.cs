e13ab3f17e242c94d47e958499b06c4daf20b24e
kkignasiak <kkignasiak.98@gmail.com>
2019-12-28 11:09:45 +0100

    adding CompanyBinding Model and order Models in Folders

---
+++ CosumeApi/Models/BindingModels/CompanyBindingModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CosumeApi.Models
+{
+    public class CompanyAddBindingModel
+    {
+        public string Name { get; set; }
+        public int Nip { get; set; }
+        public int? IndustryId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+
+        public int Id { get; set; }
+    }
+
+    public class CompanyUpdateBindingModel
+    {
+        public string Name { get; set; }
+        public int? Nip { get; set; }
+        public int? IndustryId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+
+    public class CompanyViewPublicModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Nip { get; set; }
+        public int? IndustryId { get; set; }
+        public string IdustryName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string userID { get; set; }
+    }
+}
