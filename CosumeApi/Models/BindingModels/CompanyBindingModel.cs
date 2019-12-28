1bf4949c111bdf01c98894ac3bc1439e4dc990c9
kkignasiak <kkignasiak.98@gmail.com>
2019-12-28 20:43:29 +0100

    Delete field in CompanyAddBindingModel, wchich is never used

--- CosumeApi/Models/BindingModels/CompanyBindingModel.cs
+++ CosumeApi/Models/BindingModels/CompanyBindingModel.cs
@@ -13,7 +13,6 @@ namespace CosumeApi.Models
         public string Address { get; set; }
         public string City { get; set; }
 
-        public int Id { get; set; }
     }
 
     public class CompanyUpdateBindingModel
