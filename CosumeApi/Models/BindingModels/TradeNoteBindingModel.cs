c77908ff661f4a4e1f230706976b4742dcbeb872
kkignasiak <kkignasiak.98@gmail.com>
2019-12-30 18:16:04 +0100

    add TradeNote Binding Models

---
+++ CosumeApi/Models/BindingModels/TradeNoteBindingModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CosumeApi.Models.BindingModels
+{
+    public class TradeNoteUpdateBindingModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public int CompanyId { get; set; }
+    }
+    public class TradeNoteAddBindingModel
+    {
+        public string Text { get; set; }
+
+        public int CompanyId { get; set; }
+    }
+
+    public class TradeNoteViewBindingModelModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
