f34717554337c61b9f98e2c89ce8353af8b6af29
kkignasiak <kkignasiak.98@gmail.com>
2019-12-30 18:48:04 +0100

    adding basic actions to tradeNotes

--- CosumeApi/Controllers/TradeNoteController.cs
+++ CosumeApi/Controllers/TradeNoteController.cs
@@ -14,6 +14,7 @@ namespace CosumeApi.Controllers
 {
     public class TradeNoteController : Controller
     {
+        //GET : TradeNote/1
         public async Task<ActionResult> TradeNotes(int Id)
         {
             HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync("TradeNote/" +Id.ToString());
@@ -38,5 +39,91 @@ namespace CosumeApi.Controllers
                 return View("SomethingWrong");
             }
         }
+
+        // POST : TradeNote/Create
+
+        public ActionResult CreateTradeNote()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateTradeNote(TradeNoteAddBindingModel NewTradeNote)
+        {
+            var postTask = ApiHelper.ApiClient.PostAsJsonAsync<TradeNoteAddBindingModel>("TradeNote", NewTradeNote);
+            postTask.Wait();
+
+            var result = postTask.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                //Coś z tym trza zrobić !!
+                return RedirectToAction("TradeNotes");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+                return View("NewCompany");
+            }
+        }
+
+        // PUT : Company/Update
+        public ActionResult UpdateTradeNote(int Id)
+        {
+            TradeNoteAddBindingModel Company = new TradeNoteAddBindingModel();
+            var responseTask = ApiHelper.ApiClient.GetAsync("TradeNote/" + Id.ToString());
+            responseTask.Wait();
+
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<TradeNoteAddBindingModel>();
+                readTask.Wait();
+
+                Company = readTask.Result;
+            }
+            return View(Company);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateTradeNote(TradeNoteUpdateBindingModel TradeNote)
+        {
+
+            var putTask = ApiHelper.ApiClient.PutAsJsonAsync<TradeNoteUpdateBindingModel>("TradeNote/" + TradeNote.Id.ToString(), TradeNote);
+            putTask.Wait();
+            var result = putTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("TradeNotes");
+            }
+            else
+            {
+                return View("Error");
+            }
+
+            return View(TradeNote);
+        }
+
+        // DELETE :TradeNote/Delete
+        public async Task<ActionResult> DeleteTradeNote(int Id)
+        {
+            HttpResponseMessage response = await ApiHelper.ApiClient.DeleteAsync("TradeNote/" + Id.ToString());
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                return View("Unauthorized");
+            }
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return View("Not Found");
+            }
+            else
+            {
+                return RedirectToAction("TradeNotes");
+            }
+
+
+        }
+
     }
 }
