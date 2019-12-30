7169a54b94376a7f0a09f2c4dca9e69a91745510
kkignasiak <kkignasiak.98@gmail.com>
2019-12-30 20:14:41 +0100

    add ContactPerson BindingModels

---
+++ CosumeApi/Models/BindingModels/ContactPersoBindingModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CosumeApi.Models.BindingModels
+{
+    public class ContactPersonAddBindingModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int? Phone { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Position { get; set; }
+
+        public int? CompanyId { get; set; }
+    }
+
+    public class ContactPersonUpdateBindingModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int? Phone { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Position { get; set; }
+
+        public int? CompanyId { get; set; }
+    }
+    public class ContactPersonGetBindingModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int? Phone { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Position { get; set; }
+
+        public int? CompanyId { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
