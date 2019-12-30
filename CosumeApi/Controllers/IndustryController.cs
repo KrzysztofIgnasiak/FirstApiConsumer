7afb8c1bac5defc7d66570c83e351335ce1c0caa
kkignasiak <kkignasiak.98@gmail.com>
2019-12-30 19:58:43 +0100

    adding IndustryController with Views

---
+++ CosumeApi/Controllers/IndustryController.cs
@@ -0,0 +1,38 @@
+﻿using CosumeApi.Models;
+using CosumeApi.Models.BindingModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CosumeApi.Controllers
+{
+    public class IndustryController : Controller
+    {
+        public ActionResult CreateIndustry()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCompany(IndustryBindingModel Industry)
+        {
+            var postTask = ApiHelper.ApiClient.PostAsJsonAsync<IndustryBindingModel>("Industry", Industry);
+            postTask.Wait();
+
+            var result = postTask.Result;
+
+            if (result.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Companies");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+                return View("NewIndustry");
+            }
+        }
+    }
+}
--- CosumeApi/Models/BindingModels/IndustryBindingModel.cs
+++ CosumeApi/Models/BindingModels/IndustryBindingModel.cs
@@ -7,7 +7,6 @@ namespace CosumeApi.Models.BindingModels
 {
     public class IndustryBindingModel
     {
-        public int Id { get; set; }
         public string Name { get; set; }
     }
 }
